69d97d1a4cc86670e4511b1a01aecee8748778b7
ondrej.pajgrt <ondrej.pajgrt@notino.com>
2024-05-08 16:13:26 +0200

    fix: missing files

---
+++ src/Rem.FlexiBeeSDK.Model/Agenda.cs
@@ -0,0 +1,16 @@
+namespace Rem.FlexiBeeSDK.Model;
+
+public class Agenda
+{
+    public const string Bank = "banka";
+    public const string BankAccount = "bankovni-ucet";
+    public const string References = "vazby";
+    public const string Items = "polozkyDokladu";
+    public const string ReferenceDocs = "vazebni-doklady";
+
+    public const string ReceivedInvoices = "faktura-prijata";
+    public const string IssuedInvoices = "faktura-vydana";
+    public const string BoM = "kusovnik";
+    public const string Contact = "kontakt";
+    public const string UserQuery = "uzivatelsky-dotaz";
+}
