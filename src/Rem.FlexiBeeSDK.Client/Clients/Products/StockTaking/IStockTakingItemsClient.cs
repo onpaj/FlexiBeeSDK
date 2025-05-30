25dd67085ccf5094e2772771e195c1dce70b41bc
ondrej.pajgrt <ondrej.pajgrt@notino.com>
2025-05-30 17:01:29 +0200

    fix: missing clients

---
+++ src/Rem.FlexiBeeSDK.Client/Clients/Products/StockTaking/IStockTakingItemsClient.cs
@@ -0,0 +1,12 @@
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+using Rem.FlexiBeeSDK.Model.Products.StockTaking;
+
+namespace Rem.FlexiBeeSDK.Client.Clients.Products.StockTaking;
+
+public interface IStockTakingItemsClient
+{
+    Task<IList<StockTakingItemResult>> GetStockTakingsAsync(int headerId, CancellationToken cancellationToken = default);
+    Task AddStockTakingsAsync(int headerId, int warehouseId, IEnumerable<AddStockTakingItemRequest> items, CancellationToken cancellationToken = default);
+}
