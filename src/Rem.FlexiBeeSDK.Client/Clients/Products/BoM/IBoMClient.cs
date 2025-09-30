07dadaac42786d11ec117ea1790217c94d2d6a19
pajgrtondrej <pajgrt.ondrej@gmail.com>
2025-09-30 14:18:06 +0200

    fix: bom weight in interface

--- src/Rem.FlexiBeeSDK.Client/Clients/Products/BoM/IBoMClient.cs
+++ src/Rem.FlexiBeeSDK.Client/Clients/Products/BoM/IBoMClient.cs
@@ -1,15 +1,19 @@
 ﻿using System.Collections.Generic;
 using System.Threading;
 using System.Threading.Tasks;
+using Rem.FlexiBeeSDK.Model;
+using Rem.FlexiBeeSDK.Model.Products.BoM;
 
 namespace Rem.FlexiBeeSDK.Client.Clients.Products.BoM
 {
     public interface IBoMClient
     {
-        Task<IList<Model.BoMItemFlexiDto>> GetAsync(string code, CancellationToken cancellationToken = default);
+        Task<IList<BoMItemFlexiDto>> GetAsync(string code, CancellationToken cancellationToken = default);
 
-        Task<IList<Model.BoMItemFlexiDto>> GetByIngredientAsync(string code, CancellationToken cancellationToken = default);
+        Task<IList<BoMItemFlexiDto>> GetByIngredientAsync(string code, CancellationToken cancellationToken = default);
 
         Task<bool> RecalculatePurchasePrice(int bomId, CancellationToken cancellationToken = default);
+
+        Task<ProductWeightFlexiDto?> GetBomWeight(string productCode, CancellationToken cancellationToken = default);
     }
 }
